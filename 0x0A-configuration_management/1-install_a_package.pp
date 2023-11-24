b30951a0454714a5a96da60b517a485b940c5753
Abokiey <sweneysam@gmail.com>
2023-11-24 16:11:18 +0300

    package install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # install flask from pip3 using puppet
 
+exec {'apt-get update':
+  command => 'apt-get update',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
