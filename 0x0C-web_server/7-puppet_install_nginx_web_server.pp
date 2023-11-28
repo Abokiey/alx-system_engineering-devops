8c5fd3d679b0d23346bfa4de134a090df11cd2fe
Abokiey <sweneysam@gmail.com>
2023-11-29 01:02:42 +0300

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,14 +4,14 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_from { 'install':
+file { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
 }
 
-file_to { '/var/www/html/index.html':
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
