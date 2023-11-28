4ceb787a30be30de329a87a671ffe2c3857446cd
Abokiey <sweneysam@gmail.com>
2023-11-29 01:13:36 +0300

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,39 @@
-# Automating nginx configuration using puppet
+# nginx_config.pp
 
+# Install Nginx
 package { 'nginx':
   ensure => installed,
 }
 
-file { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
 service { 'nginx':
   ensure  => running,
+  enable  => true,
   require => Package['nginx'],
 }
+
+#config files
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx_config/nginx.conf.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
+}
+
+# Template 
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx_config/nginx.conf.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Notify service restart if configuration file changes
+File['/etc/nginx/sites-available/default'] ~> Service['nginx']
+
