2f29e0442bdad9e5d1574558014bfcaab5986fb7
Abokiey <sweneysam@gmail.com>
2024-01-16 13:16:58 +0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,12 @@ def number_of_subscribers(subreddit):
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     user_agent = 'Mozilla/5.0'
-    headers = {'user-agent' : user_agent}
+    headers = {'User-Agent': user_agent}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+
+    if response.status_code != 200:
         return 0
+
+    results = response.json()["data"]["subscribers"]
+    return results
