207f4cd5562a9a88540d12e9b0a92910569b4773
Abokiey <sweneysam@gmail.com>
2024-01-17 18:41:28 +0300

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,25 @@ def top_ten(subreddit):
     """prints top 10 posts for a given subbreddit"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
-    user_agent = 'Mozilla/5.0'
-    headers = {'user_agent': user_agent}
+    headers = {
+            'User-Agent': 'Mozilla/5.0'
+    }
+    params = {
+            'limit': 10
+    }
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
     if response.status_code != 200:
-        print('None')
+        print(None)
         return
 
-    results = response.json()['data']
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    res = response.json()
+    hot_posts = res['data']['children']
+    if len(hot_posts) == 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
