78ef4b738fb602b4aeb73460e62454dcf85990f2
Abokiey <sweneysam@gmail.com>
2024-01-18 10:56:22 +0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     user_agent = 'Mozilla/5.0'
     headers = {'User-Agent': user_agent}
